ac8c1abf0ce3a5a3ab4ab299c0aea6b53c86c4d7
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2023-12-12 18:15:32 +0100

    Proper SGX Andromeda kettle contract

---
+++ src/Andromeda.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.8;
+
+import {IAndromeda} from "src/IAndromeda.sol";
+import {DcapDemo} from "src/DcapVerifier.sol";
+import {V3Struct} from "automata-dcap-v3-attestation/lib/QuoteV3Auth/V3Struct.sol";
+import {V3Parser} from "automata-dcap-v3-attestation/lib/QuoteV3Auth/V3Parser.sol";
+
+contract Andromeda is IAndromeda, DcapDemo {
+    constructor(address sigVerifyLib) DcapDemo(sigVerifyLib) {}
+
+    address public constant ATTEST_ADDR = 0x0000000000000000000000000000000000040700;
+    address public constant VOLATILESET_ADDR = 0x0000000000000000000000000000000000040701;
+    address public constant VOLATILEGET_ADDR = 0x0000000000000000000000000000000000040702;
+    address public constant RANDOM_ADDR = 0x0000000000000000000000000000000000040703;
+
+    function volatileSet(bytes32 key, bytes32 value) external override {
+        bytes memory cdata = abi.encodePacked([key, value]);
+        (bool success, bytes memory _out) = VOLATILESET_ADDR.staticcall(cdata);
+        _out;
+        require(success);
+    }
+
+    function volatileGet(bytes32 key) external override returns (bytes32) {
+        (bool success, bytes memory value) = VOLATILEGET_ADDR.staticcall(abi.encodePacked((key)));
+        require(success);
+        require(value.length == 32);
+        return abi.decode(value, (bytes32));
+    }
+
+    function attestSgx(bytes32 userdata) external override returns (bytes memory) {
+        (bool success, bytes memory attestBytes) = ATTEST_ADDR.staticcall(abi.encodePacked(userdata));
+        require(success);
+        return attestBytes;
+    }
+
+    function verifySgx(address caller, bytes32 appData, bytes memory att) public view returns (bool) {
+        bytes memory userReport = abi.encodePacked(bytes32(bytes20(caller)), appData);
+        (,, V3Struct.EnclaveReport memory r,,) = V3Parser.parseInput(att);
+        if (keccak256(r.reportData) != keccak256(userReport)) {
+            return false;
+        }
+        return this.verifyAttestation(att);
+    }
+
+    function localRandom() external view override returns (bytes32) {
+        (bool success, bytes memory randomBytes) = RANDOM_ADDR.staticcall("");
+        require(success);
+        require(randomBytes.length == 32);
+        return bytes32(randomBytes);
+    }
+}
