62af96ba8313d173601d60e72d61a2b5bf761809
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2023-12-21 19:17:36 +0100

    Adds configuration step that uploads all tcbinfos

--- Makefile
+++ Makefile
@@ -4,5 +4,11 @@ build:
 
 .PHONY: deploy
 deploy: build
-	# NOTE: for deployment RAW_PRIVATE_KEY is needed!
 	npx ts-node -T scripts/deploy.ts
+
+.PHONY: configure_all_tcbinfos
+configure_all_tcbinfos:
+	# Non-PHONY! If needed, clear it manually
+	cd lib/sgx-tcbInfos && make
+	export TCB_INFO_FILES="$(shell find ./lib/sgx-tcbInfos/assets -name "tcbinfo.json" -printf "%p ")"; \
+	npx ts-node -T scripts/configure_tcbinfo.ts
---
+++ scripts/configure_tcbinfo.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import { ethers, JsonRpcProvider } from "ethers";
+
+import { TCBInfoStruct } from "lib/automata-dcap-v3-attestation/typechain-types/contracts/AutomataDcapV3Attestation";
+
+import { LocalConfig, attach_artifact, ADDR_OVERRIDES } from "./common.ts"
+
+async function main() {
+  const tcbInfoFiles = process.env.TCB_INFO_FILES.trim().split(" ");
+
+  const provider = new JsonRpcProvider(LocalConfig.RPC_URL);
+  const wallet = new ethers.Wallet(LocalConfig.PRIVATE_KEY, provider);
+
+  const Andromeda = await attach_artifact(LocalConfig.ANDROMEDA_ARTIFACT, wallet, ADDR_OVERRIDES[LocalConfig.ANDROMEDA_ARTIFACT]);
+
+  for (const tcbInfoFile in tcbInfoFiles) {
+    const tcbInfo = JSON.parse(fs.readFileSync(tcbInfoFiles[tcbInfoFile], 'utf8')) as TCBInfoStruct.TCBInfoStruct.tcbInfo;
+
+    const isAlreadyDeployed = (await Andromeda.tcbInfo(tcbInfo.fmspc))[1] === tcbInfo.fmspc;
+    if (!isAlreadyDeployed) {
+      const tcbInfoTx = await (await Andromeda.configureTcbInfoJson(tcbInfo.fmspc, tcbInfo)).wait();
+      console.log("configured "+tcbInfo.fmspc+" in "+tcbInfoTx.hash);
+    } else {
+      console.log(tcbInfo.fmspc+" already configured");
+    }
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
