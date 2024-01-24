52497ea18e7a43a150a298d6362353ad60bbc2c6
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2024-01-24 18:04:08 +0100

    Prettier demo

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,8 +3,10 @@ import flashbotsLogo from './assets/flashbots.png'
 import './App.css'
 import { 
   decodeFunctionResult,
+  decodeFunctionData,
   encodeFunctionData,
   fromHex,
+  getAbiItem,
   getContract,
   HttpTransport,
   http,
@@ -32,25 +34,24 @@ function App() {
   const [consoleLog, setConsoleLog] = useState<string>("");
 
   function updateConsoleLog(newLog: string) {
-    setConsoleLog(consoleLog => "["+Date.now().toString()+"]: "+newLog+"\n"+consoleLog);
+    const currentTime = (new Date()).toLocaleString("en-US", {hour: "numeric", minute: "numeric", second: "numeric", hour12: false});
+    setConsoleLog(consoleLog => log_font_color("lightgrey", "["+currentTime+"]: ")+newLog+"<br>"+consoleLog);
   }
 
-  // only runs once
   useEffect(() => {
-    setConsoleLog("");
-
     const suaveProvider = getSuaveProvider(http(LocalConfig.RPC_URL))
     updateConsoleLog("Connected to SUAVE Rigil RPC at "+LocalConfig.RPC_URL);
-    setSuaveProvider(suaveProvider)
+    setSuaveProvider(suaveProvider);
 
     const suaveWallet = getSuaveWallet({
       transport: http(LocalConfig.RPC_URL),
       privateKey: LocalConfig.PRIVATE_KEY as '0x{string}',
-    })
-    updateConsoleLog("Opened SUAVE wallet "+suaveWallet.account.address);
+    });
+
+    updateConsoleLog("Opened SUAVE wallet "+format_explorer_link(suaveWallet.account.address));
 
     // Create contract instance
-    let ADDR_OVERRIDES: {[key: string]: any} = LocalConfig.ADDR_OVERRIDES;
+    const ADDR_OVERRIDES: {[key: string]: any} = LocalConfig.ADDR_OVERRIDES;
     const timelock = getContract({
       address: ADDR_OVERRIDES[LocalConfig.TIMELOCK_ARTIFACT],
       abi: Timelock.abi,
@@ -58,27 +59,48 @@ function App() {
       walletClient: suaveWallet,
     });
     setTimelock(timelock);
-    updateConsoleLog("Connected to Timelock contract at "+timelock.address);
+    updateConsoleLog("Connected to Timelock contract at "+format_explorer_link(timelock.address));
 
-    checkTimelockIsInitialized()
+    return () => {
+      setConsoleLog("");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isTimelockInitialized) {
+      return
+    }
+
+    checkTimelockIsInitialized();
+    const timerCheckTimelockIsInitialized = setInterval(async () => {
+      await checkTimelockIsInitialized();
+    }, 1000); // Update every second
+
+    // Clean up the interval on component unmount
+    return () => {
+      clearInterval(timerCheckTimelockIsInitialized);
+    };
+  }, [isTimelockInitialized]);
+
+  useEffect(() => {
+    if (!encryptedMessage || decryptedMessage) {
+      return;
+    }
 
     const deadlineFetch = setInterval(async () => {
-      if (encryptedMessage && !decryptedMessage) {
-        await fetchDeadline();
-      }
+      await fetchDeadline();
     }, 1000); // Update every second
-    //
+
     const decryptionAttempt = setInterval(async () => {
-      if (encryptedMessage && !decryptedMessage) {
-        try {
-          const message = await decryptMessage(encryptedMessage)
-          setDecryptedMessage(message);
-          updateConsoleLog("Decrypted message is: "+message);
-          clearInterval(deadlineFetch);
-          clearInterval(decryptionAttempt);
-        } catch(e) {
-          updateConsoleLog("Failed to decrypt message "+deadline+" seconds before deadline: "+e.message);
-        }
+      const timeout = await fetchDeadline();
+      try {
+        const message = await decryptMessage(encryptedMessage)
+        setDecryptedMessage(message);
+        updateConsoleLog("Decrypted message is: "+message);
+        clearInterval(deadlineFetch);
+        clearInterval(decryptionAttempt);
+      } catch(e) {
+        updateConsoleLog("Failed to decrypt message "+timeout?.toString()+' seconds before deadline: '+log_font_color("red", e.message));
       }
     }, 10000); // Try to decrypt every 10 seconds
 
@@ -87,10 +109,14 @@ function App() {
       clearInterval(deadlineFetch);
       clearInterval(decryptionAttempt);
     }
-  }, [encryptedMessage]); // Re-run the effect when `encryptedMessage` changes
+  }, [decryptedMessage, encryptedMessage]); // Re-run the effect when `encryptedMessage` changes
 
   async function checkTimelockIsInitialized() {
-    const isInitialized = await timelock?.read.isInitialized()
+    if (isTimelockInitialized) {
+      return;
+    }
+
+    const isInitialized = await timelock?.read.isInitialized();
     if (isInitialized) {
       console.log("Timelock is initialized")
       setIsTimelockInitialized(true)
@@ -106,8 +132,7 @@ function App() {
       toHex(crypto.getRandomValues(new Uint8Array(32)))
     ]);
     const tx = await timelock.write.submitEncrypted([encryptedMessage])
-    window.tx = tx;
-    updateConsoleLog("Submited encrypted message to the chain: "+JSON.stringify(tx));
+    updateConsoleLog("Submited encrypted message to the chain: "+format_explorer_link(tx));
 
     return encryptedMessage
   }
@@ -138,27 +163,28 @@ function App() {
 
     await kettle_advance(server);
 
+    const decryptAbi = getAbiItem({abi: timelock.abi, name: "decrypt"});
     const data = encodeFunctionData({
-      abi: timelock.abi,
-      functionName: "decrypt",
+      abi: [decryptAbi],
       args: [
         encryptedMessage,
       ],
     })
-    updateConsoleLog("Requesting decryption from kettle: "+JSON.stringify(data));
-    let resp = await kettle_execute(server, timelock.address, data.toString());
 
-    let executionResult = JSON.parse(resp);
+    const resp = await kettle_execute(server, timelock.address, data.toString());
+    const callLog = pretty_print_contract_call(decryptAbi, [encryptedMessage]);
+
+    const executionResult = JSON.parse(resp);
     if (executionResult.Success === undefined) {
-      updateConsoleLog("Kettle refused to decrypt the message: "+JSON.stringify(resp));
-      throw("execution did not succeed: "+JSON.stringify(resp));
+      updateConsoleLog("Kettle refused to decrypt the message: "+callLog+log_font_color("red", " error: \""+format_revert_string(executionResult.Revert.output)+"\""));
+      throw new Error("execution did not succeed: "+format_revert_string(executionResult.Revert.output));
     }
-    updateConsoleLog("Kettle decrypted the message: "+JSON.stringify(resp));
 
-    // @ts-expect-error
+    const callResLog = pretty_print_contract_result(decryptAbi, executionResult.Success.output.Call);
+    updateConsoleLog("Requested decryption from kettle: "+callLog+" -> "+callResLog);
+
     const offchainDecryptResult = decodeFunctionResult({
-      abi: timelock.abi,
-      functionName: "decrypt",
+      abi: [decryptAbi],
       data: executionResult.Success.output.Call,
     }) as `0x${string}`;
     console.log("Successfully decrypted message: '"+fromHex(offchainDecryptResult, "string").trim()+"'");
@@ -182,10 +208,10 @@ function App() {
         { !messagePromptHidden && (
           <button onClick={async () => {
             if (isTimelockInitialized != true) {
-              await checkTimelockIsInitialized()
+              await checkTimelockIsInitialized();
             } else if (encryptedMessage === undefined) {
               setMessagePromptHidden(true);
-              setEncryptedMessage(await submitEncryptedMessage(message))
+              setEncryptedMessage(await submitEncryptedMessage(message));
             } else {
               setDecryptedMessage(await decryptMessage(encryptedMessage));
               updateConsoleLog("Decrypted message is: "+message);
@@ -204,14 +230,14 @@ function App() {
           )}
           {messagePromptHidden && deadline === 0n && (
             <div>
-            {decryptedMessage === undefined ? "Decrypting message..." : "Decrypted message: '"+decryptedMessage+"'"}
+            {decryptedMessage === undefined ? "Decrypting message..." : "Decrypted message: "+decryptedMessage+""}
             </div>
           )}
 
         <br/>
         <br/>
-        <textarea rows="15" cols="96" value={consoleLog} readOnly>
-        </textarea>
+        <div className="textbox" dangerouslySetInnerHTML={{__html: consoleLog}}>
+        </div>
       </div>
     </>
   )
@@ -219,6 +245,63 @@ function App() {
 
 function assert(condition: unknown, msg?: string): asserts condition {
   if (condition === false) throw new Error(msg)
+}
+
+function pretty_print_contract_call(abi, inputs) {
+  assert(abi.inputs.length == inputs.length)
+
+  const fn = abi.type+" "+abi.name;
+  let args = "";
+  for (let i = 0; i < abi.inputs.length; i++) {
+    args += abi.inputs[i].type + " " + abi.inputs[i].name+": "+inputs[i];
+    if (i+1 != abi.inputs.length) {
+      args += ", ";
+    }
+  }
+
+  return fn + "("+args+")";
+}
+
+function pretty_print_contract_result(methodAbi, rawOutput: '0x{string}') {
+    const decodedResult = decodeFunctionResult({
+      abi: [methodAbi],
+      data: rawOutput,
+    });
+
+    if (decodedResult == null) {
+      throw new Error("unable to decode result");
+    }
+
+    if (typeof decodedResult != "object") {
+      assert(methodAbi.outputs.length == 1);
+      return "["+methodAbi.outputs[0].type + " " + methodAbi.outputs[0].name + ": "+decodedResult+"]";
+    }
+
+  let res = "[";
+  for (let i = 0; i < methodAbi.outputs.length; i++) {
+    res += methodAbi.outputs[i].type + " " + methodAbi.outputs[i].name + ": "+decodedResult[methodAbi.outputs[i].name];
+    if (i+1 != methodAbi.outputs.length) {
+      res += ", ";
+    }
+  }
+
+  res += "]";
+  return res;
+}
+
+function format_revert_string(rawOutput: '0x{string}') {
+  return decodeFunctionData({
+    abi: [{inputs: [{ name: "error", type: "string" }], name: "Error", type: "function"}],
+    data: rawOutput,
+  }).args[0];
+}
+
+function log_font_color(color: string, str: string) {
+  return '<font color="'+color+'">'+str+'</font>'
+}
+
+function format_explorer_link(address: '0x{string}') {
+  return '<a target="_blank" href=https://explorer.rigil.suave.flashbots.net/address/'+address+'>'+address+'</a>';
 }
 
 export default App
