2f6ec47e9a2769f8dae4fe192ef2823081ec7313
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2024-01-25 01:44:48 +0100

    Adds Metamask signing for chain transctions

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,5 +1,21 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        dappMetadata: {
+          name: "Example React Dapp",
+          url: window.location.href,
+        },
+        // Other options
+      }}
+    ></MetaMaskProvider>
+    <App />
+  </>,
+);
